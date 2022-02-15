6452ba23521eb15cab45f4249abb7e920cdbaaa6
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-15 20:35:05 +0500

    sidebar completed

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,6 +3,15 @@ import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
+	const recentItem = (topic) => {
+		return (
+			<div className="sidebar__recentItem">
+				<span className="sidebar__hash">#</span>
+				<p>{topic}</p>
+			</div>
+		);
+	};
+
 	return (
 		<div className="sidebar">
 			<div className="sidebar__top">
@@ -28,6 +37,11 @@ function Sidebar() {
 
 			<div className="sidebar__bottom">
 				<p>Recent</p>
+				{recentItem("reactjs")}
+				{recentItem("programming")}
+				{recentItem("softwareengineering")}
+				{recentItem("design")}
+				{recentItem("developer")}
 			</div>
 		</div>
 	);
