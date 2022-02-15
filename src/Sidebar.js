c39ca882ef0860fb13a36c535d37fd5009d84dc2
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-15 13:46:49 +0500

    continue

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,34 @@
+import { Avatar } from "@mui/material";
 import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
 	return (
 		<div className="sidebar">
-			<h1>sidebar</h1>
+			<div className="sidebar__top">
+				<img
+					src="https://images.pexels.com/photos/1568607/pexels-photo-1568607.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+					alt="background img"
+				/>
+				<Avatar className="sidebar__avatar" />
+				<h2>Jaffer Sunny</h2>
+				<h4>jaffer.sunny125@gmail.com</h4>
+			</div>
+
+			<div className="sidebar__stats">
+				<div className="sidebar__stat">
+					<p>Who viewed you</p>
+					<p className="sidebar__statNumber">2,534</p>
+				</div>
+				<div className="sidebar__stat">
+					<p>Views on post</p>
+					<p className="sidebar__statNumber">2,534</p>
+				</div>
+			</div>
+
+			<div className="sidebar__bottom">
+				<p>Recent</p>
+			</div>
 		</div>
 	);
 }
