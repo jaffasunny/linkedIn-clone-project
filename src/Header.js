cc0f7386af70365418077e4953972a334b3e9bbe
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-15 13:18:39 +0500

    Building Sidebar component

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "./Header.css";
-import { Search } from "@mui/icons-material";
+import {
+	BusinessCenter,
+	Chat,
+	Home,
+	Search,
+	SupervisorAccount,
+	Notifications,
+} from "@mui/icons-material";
+import HeaderOptions from "./HeaderOptions";
 
 function Header() {
 	return (
@@ -16,7 +24,17 @@ function Header() {
 					<input type="text" />
 				</div>
 			</div>
-			<div className="header__right">right</div>
+			<div className="header__right">
+				<HeaderOptions Icon={Home} title="Home" />
+				<HeaderOptions Icon={SupervisorAccount} title="My Network" />
+				<HeaderOptions Icon={BusinessCenter} title="My Network" />
+				<HeaderOptions Icon={Chat} title="My Network" />
+				<HeaderOptions Icon={Notifications} title="My Network" />
+				<HeaderOptions
+					avatar="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+					title="me"
+				/>
+			</div>
 		</div>
 	);
 }
