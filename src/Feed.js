b279532cfd9fb94e39f4aba850b035be066f27cd
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-17 15:08:03 +0500

    Added posting feature in Feed component

--- src/Feed.js
+++ src/Feed.js
@@ -5,20 +5,61 @@ import {
 	Image,
 	Subscriptions,
 } from "@mui/icons-material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Feed.css";
+import { db } from "./firebase";
 import InputOption from "./InputOption";
 import Post from "./Post";
+import firebase from "firebase";
 
 function Feed() {
+	const [input, setInput] = useState("");
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		db.collection("posts")
+			.orderBy("timestamp", "desc")
+			.onSnapshot((snapshot) => {
+				setPosts(
+					snapshot.docs.map((doc) => {
+						return {
+							id: doc.id,
+							data: doc.data(),
+						};
+					})
+				);
+			});
+	}, []);
+
+	const sendPost = (e) => {
+		e.preventDefault();
+
+		db.collection("posts").add({
+			name: "Jaffer Sunny",
+			description: "This is a test",
+			message: input,
+			photoUrl: "",
+			timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+		});
+	};
+
 	return (
 		<div className="feed">
 			<div className="feed__inputContainer">
 				<div className="feed__input">
 					<Create />
 					<form>
-						<input type="text" className="form__input" />
-						<button type="submit">Send</button>
+						<input
+							value={input}
+							onChange={(e) => {
+								setInput(e.target.value);
+							}}
+							type="text"
+							className="form__input"
+						/>
+						<button onClick={sendPost} type="submit">
+							Send
+						</button>
 					</form>
 				</div>
 				<div className="feed__inputOptions">
@@ -33,11 +74,17 @@ function Feed() {
 				</div>
 			</div>
 
-			<Post
-				name="Jaffer Sunny"
-				description="This is a test"
-				message="WOW this worked"
-			/>
+			{posts.map(({ id, data: { name, description, message, photoUrl } }) => {
+				return (
+					<Post
+						key={id}
+						name={name}
+						description={description}
+						message={message}
+						photoUrl={photoUrl}
+					/>
+				);
+			})}
 		</div>
 	);
 }
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/";
+
+const firebaseApp = firebase.initializeApp({
+	apiKey: "AIzaSyD6GsPxAcPeFKkJB3b4TnTy0jvqF4SVuPg",
+	authDomain: "linkedin-clone-project-30b13.firebaseapp.com",
+	projectId: "linkedin-clone-project-30b13",
+	storageBucket: "linkedin-clone-project-30b13.appspot.com",
+	messagingSenderId: "605627362948",
+	appId: "1:605627362948:web:0cdd28cd3300d388ec5cd1",
+});
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
