04ba687bc313124904cff96cb81dd655ff97becc
Syed Jaffer Sunny <52747254+jaffasunny@users.noreply.github.com>
2022-08-09 13:46:45 +0500

    final commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/";
 
 const firebaseApp = firebase.initializeApp({
-	apiKey: "AIzaSyD6GsPxAcPeFKkJB3b4TnTy0jvqF4SVuPg",
-	authDomain: "linkedin-clone-project-30b13.firebaseapp.com",
-	projectId: "linkedin-clone-project-30b13",
-	storageBucket: "linkedin-clone-project-30b13.appspot.com",
-	messagingSenderId: "605627362948",
-	appId: "1:605627362948:web:0cdd28cd3300d388ec5cd1",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APPID,
 });
 
 const db = firebaseApp.firestore();
